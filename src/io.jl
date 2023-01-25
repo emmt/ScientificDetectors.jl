a1473b4a1b36d6948b45ac97933b32e12ad44dad
buthanoid <portail@buthanoid.eu>
2023-01-25 15:35:04 +0100

    convenience headers for reduced calib file

--- src/io.jl
+++ src/io.jl
@@ -154,6 +154,18 @@ function write(io::FitsIO, obj::ReducedCalibration{T,N},
     hdr["HDUNAME"] = (name, "reduced detector calibration")
     hdr["HDUVERS"] = (vers, "version of this format")
     merge!(hdr, DetectorAxes(obj))
+
+    # convenience headers for easier exploration of ReducedCalibration FITS files (i.e with fv,DS9,..)
+    hdr["FRAME1"] = "co-log-likelihood (f)"
+    hdr["FRAME2"] = "constant bias (z) (in ADU)"
+    hdr["FRAME3"] = "detector gain (g) (in electron/ADU)"
+    hdr["FRAME4"] = "std of readout-noise (sigma) (in ADU/frame)"
+    hdr["FRAME5"] = "good pixel map (gpm) (1=goodpixel)"
+    for k ∈ eachindex(obj.src)
+        hdr[string("FRAME",5+k)] = string(obj.src[k], " (in ADU/s)")
+    end
+
+    # mandatory headers to eventually read() the ReducedCalibration FITS file
     for k ∈ eachindex(obj.src)
         hdr[string("SRC",k)] = obj.src[k]
     end
