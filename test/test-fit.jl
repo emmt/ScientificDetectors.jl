9606e7731c642a737cb2775beaff2f202d9fcf21
buthanoid <portail@buthanoid.eu>
2026-06-02 14:38:53 +0200

    remise de DARK, et round

--- test/test-fit.jl
+++ test/test-fit.jl
@@ -10,7 +10,7 @@ g = 1.9
 z = 10
 
 function sim(Δt, σ, z, src...)
-    sum(s -> rand(Poisson(s * Δt)), src) * g + rand(Normal(z,σ))
+    round(sum(s -> rand(Poisson(s * Δt)), src) * g + rand(Normal(z,σ)), RoundNearestTiesUp)
 end
 
 roi = DetectorAxes(1:1)
@@ -18,22 +18,20 @@ roi = DetectorAxes(1:1)
 l_z = Float32[]
 l_g = Float32[]
 l_σ = Float32[]
-#l_src_dark = Float32[]
+l_src_dark = Float32[]
 l_src_flat = Float32[]
 
-@showprogress for n in 1:500
+@showprogress for n in 1:1000
 
     calib_data = CalibrationData{T}(roi, [
-            CalibrationCategory("FLAT", :(flat))
-#            CalibrationCategory("FLAT", :(flat + dark)),
-#            CalibrationCategory("DARK", :(dark))
+            CalibrationCategory("FLAT", :(flat + dark)),
+            CalibrationCategory("DARK", :(dark))
         ])
 
     for Δt in T[ 1.0, 3.0, 7.0, 10.0, 30.0, 100.0 ]
         for f in 1:1000
-            push!(calib_data, CalibrationDataFrame{T,N}("FLAT", Δt, [sim(Δt, σ, z, src_flat)] ))
-#            push!(calib_data, CalibrationDataFrame{T,N}("FLAT", Δt, [sim(Δt, σ, z, src_flat, src_dark)] ))
-#            push!(calib_data, CalibrationDataFrame{T,N}("DARK", Δt, [sim(Δt, σ, z, src_dark)] ))
+            push!(calib_data, CalibrationDataFrame{T,N}("FLAT", Δt, [sim(Δt, σ, z, src_flat, src_dark)] ))
+            push!(calib_data, CalibrationDataFrame{T,N}("DARK", Δt, [sim(Δt, σ, z, src_dark)] ))
         end
     end
 
@@ -42,13 +40,13 @@ l_src_flat = Float32[]
     push!(l_z, r.z[1])
     push!(l_g, r.g[1])
     push!(l_σ, r.σ[1])
-#    push!(l_src_dark, r.s[findfirst(==("dark"),r.src)][1])
+    push!(l_src_dark, r.s[findfirst(==("dark"),r.src)][1])
     push!(l_src_flat, r.s[findfirst(==("flat"),r.src)][1])
 end
 
 @show z, mean(l_z), std(l_z);
 @show g, mean(l_g), std(l_g);
 @show σ, mean(l_σ), std(l_σ);
-#@show (src_dark * g), mean(l_src_dark), std(l_src_dark);
+@show (src_dark * g), mean(l_src_dark), std(l_src_dark);
 @show (src_flat * g), mean(l_src_flat), std(l_src_flat);
 
